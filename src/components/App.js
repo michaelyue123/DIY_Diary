35df7ef51c902b815da5a901500d132250bfd64e
Michael Yue <45374025+michaelyue123@users.noreply.github.com>
2020-03-12 23:43:27 +1100

    Login feature update

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Login from './Login';
+import Navbar from './Navbar';
 
 
 class App extends React.Component {
     render() {
         return (
-            <div className="ui container">
+            <div>
+                <Navbar />
                 <Login />
             </div>
         );
