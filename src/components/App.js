c5409d584e71eb6c914455a25eff96b687e2d269
Michael Yue <45374025+michaelyue123@users.noreply.github.com>
2020-03-12 22:17:02 +1100

    Login Feature

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Login from './Login';
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="ui container">
+                <Login />
+            </div>
+        );
+    }
+}
+
+export default App;
+
