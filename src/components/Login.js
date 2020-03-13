184bee9d664bff52b650a1f73bb4832265bf0364
Michael Yue <45374025+michaelyue123@users.noreply.github.com>
2020-03-14 08:25:16 +1100

    Login Feature Update

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,14 +2,24 @@ import React from 'react';
 import './Login.css';
 
 class Login extends React.Component {
-    state = { email:'', password:''};
+    state = { email:'', password:'', hidden:true };
 
+    handleEmailChange = e => {
+        this.setState({ email: e.target.value });
+    }
+
+    handlePasswordChange = e => {
+        this.setState({ password: e.target.value });
+    }
+
+    toggleShow =() => {
+        this.setState({ hidden: !this.state.hidden });
+    }
 
     // call back function
     onFormSubmit = event => {
         event.preventDefault();
 
-        // this.setState({ email:  })
     }
 
     render() {
@@ -20,11 +30,28 @@ class Login extends React.Component {
                     <form className="ui form">
                         <div className="field">
                             <label>email</label>
-                            <input type="text" name="email" placeholder="enter your email address" required/>
+                            <input 
+                            type="text" 
+                            value={this.state.email} 
+                            onChange={this.handleEmailChange} 
+                            name="email" 
+                            placeholder="enter your email address" required
+                            />
                         </div>
                         <div className="field">
                             <label>password</label>
-                            <input type="text" name="password" placeholder="enter your password" required/>
+                            <input 
+                            type={this.state.hidden ? "password" : "text"} 
+                            value={this.state.password} 
+                            onChange={this.handlePasswordChange} 
+                            name="password" 
+                            placeholder="enter your password" 
+                            required
+                            />
+                        </div>
+                        <div className="form-check">
+                            <input type="checkbox" onClick={this.toggleShow} class="form-check-input" id="exampleCheck1" />
+                            <label class="form-check-label" for="exampleCheck1">show password</label>
                         </div>
                         <button className="ui button" type="submit" onSubmit={this.onFormSubmit}>Submit</button>
                     </form>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,39 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 
 class Navbar extends React.Component {
+
+    state = { redirect: false };
+
+    setRedirect = () => {
+        this.setState({
+            redirect: true
+        });
+    }
+
+    renderRedirect = () => {
+        if(this.state.redirect) {
+            return <Redirect to='' />;
+        }
+    }
+
+
     render() {
         return(
-            <div>
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="/#">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav">
-                            <a class="nav-item nav-link active" href="/#">Home <span class="sr-only">(current)</span></a>
-                            <a class="nav-item nav-link" href="/#">Sign up</a>
-                            <a class="nav-item nav-link" href="/#">Sign in</a>
-                        </div>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand" href="/#">DIY_Diary</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-item nav-link" onClick={this.setRedirect} href="/#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-item nav-link" onClick={this.setRedirect} href="/#">Register</a>
+                        <a class="nav-item nav-link" onClick={this.setRedirect} href="/#">Sign in</a>
                     </div>
-                </nav>
-            </div>
+                </div>
+            </nav>
         );
     }
 }
