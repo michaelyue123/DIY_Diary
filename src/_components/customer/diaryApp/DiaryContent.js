519a84dca17cae243d3f1a453e0a8a00026e67d4
Michael Yue <45374025+michaelyue123@users.noreply.github.com>
2020-05-03 23:35:19 +1000

    Diary Update

--- src/_components/customer/diaryApp/DiaryContent.js
+++ src/_components/customer/diaryApp/DiaryContent.js
@@ -1,10 +1,56 @@
 import React, { Component } from 'react'
+import '../../styles/customer/Diary/DiaryContent.css';
+import { FormControl, InputGroup, Form } from 'react-bootstrap';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 
 export class DiaryContent extends Component {
+    
+    state = {
+        startDate: new Date()
+    };
+
+    handleChange = date => {
+        this.setState({
+            startDate: date
+        });
+    };
+
+
     render() {
         return (
-            <div>
-                <h1 className="diary content"> This is the diary content page!</h1>
+            <div >
+                <div className="diary content">
+                    <InputGroup className="mb-3">
+                        <FormControl
+                            className="inputbox"
+                            placeholder="Entry Title"
+                            aria-label="Title"
+                            aria-describedby="basic-addon1"
+                        />
+                    </InputGroup>
+                    <Form.Group controlId="exampleForm.SelectCustom">
+                        <i className="calendar check outline icon"></i>
+                        <Form.Label style={{ fontSize: "1.3vw" }}>Select Date</Form.Label>
+                        <DatePicker 
+                            selected={this.state.startDate}
+                            onChange={this.handleChange}
+                            className="date"
+                        />
+                    </Form.Group>
+                    <Form.Control className="textbox" placeholder="Your entry here" as="textarea" rows="17" />
+                    <div className="content button">
+                        <Button className="ui button" id="personal" type="submit">
+                            <Link><span id="update">Save</span></Link>
+                        </Button>
+                        <Button className="ui button" id="personal" type="submit">
+                            <Link><span id="update">Cancel</span></Link>
+                        </Button>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/_components/customer/diaryApp/MyDiary.js
+++ src/_components/customer/diaryApp/MyDiary.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import '../../styles/Diary/MyDiary.css';
-import { history } from '../../../_helpers/history';
+import '../../styles/customer/Diary/MyDiary.css';
+// import { history } from '../../../_helpers/history';
 
 class MyDiary extends Component {
 
     render() {
         return (
             <div>
-                <div onClick={() => history.push('/diaryContent')} className="app container">
+                <div onClick={() => this.props.history.push('/diaryContent')} className="app container">
                     <h1 className="diary title">diary</h1>
                 </div>
             </div>
